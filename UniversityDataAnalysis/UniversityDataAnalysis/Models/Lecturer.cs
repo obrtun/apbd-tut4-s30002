544417c379c478ebf7f2f022b690193eb3407034
Vladyslav Obertun <obrtun@gmail.com>
2026-03-30 20:57:47 +0200

    Add models

---
+++ UniversityDataAnalysis/UniversityDataAnalysis/Models/Lecturer.cs
@@ -0,0 +1,12 @@
+﻿namespace UniversityDataAnalysis.Models;
+
+public class Lecturer
+{
+    public int Id { get; set; }
+    public string FullName { get; set; }
+    
+    public override string ToString()
+    {
+        return $"{Id}: {FullName}";
+    }
+}
---
+++ UniversityDataAnalysis/UniversityDataAnalysis/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿namespace UniversityDataAnalysis.Models;
+
+public class Student
+{
+    public int Id { get;  set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string City { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Id}: {FirstName} {LastName}, {City}, {Email}";
+    }
+}
