d014e2425ede77a53f14a1dad54c2244bb456fd8
Vladyslav Obertun <obrtun@gmail.com>
2026-03-30 21:27:27 +0200

    Add Data, basic menu, show all data function

---
+++ UniversityDataAnalysis/UniversityDataAnalysis/Data/UniversityData.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using UniversityDataAnalysis.Models;
+
+namespace UniversityDataAnalysis.Data;
+
+public static class UniversityData
+    {
+        public static List<Student> Students { get; } = new List<Student>
+        {
+            new Student
+            {
+                Id = 1,
+                FirstName = "Anna",
+                LastName = "Kowalska",
+                City = "Warsaw",
+                Email = "anna@uni.test"
+            },
+            new Student
+            {
+                Id = 2,
+                FirstName = "Piotr",
+                LastName = "Nowak",
+                City = "Krakow",
+                Email = "piotr@uni.test"
+            },
+            new Student
+            {
+                Id = 3,
+                FirstName = "Maria",
+                LastName = "Wisniewska",
+                City = "Warsaw",
+                Email = "maria@uni.test"
+            },
+            new Student
+            {
+                Id = 4,
+                FirstName = "Jan",
+                LastName = "Zielinski",
+                City = "Gdansk",
+                Email = "jan@uni.test"
+            },
+            new Student
+            {
+                Id = 5,
+                FirstName = "Ola",
+                LastName = "Wojcik",
+                City = "Poznan",
+                Email = "ola@uni.test"
+            }
+        };
+
+        public static List<Lecturer> Lecturers { get; } = new List<Lecturer>
+        {
+            new Lecturer
+            {
+                Id = 1,
+                FullName = "Dr. Adam Nowicki"
+            },
+            new Lecturer
+            {
+                Id = 2,
+                FullName = "Prof. Ewa Maj"
+            },
+            new Lecturer
+            {
+                Id = 3,
+                FullName = "Dr. Tomasz Lewandowski"
+            }
+        };
+
+        public static List<Course> Courses { get; } = new List<Course>
+        {
+            new Course
+            {
+                Id = 1,
+                Name = "Databases",
+                StartDate = new DateTime(2026, 4, 5),
+                LecturerId = 1
+            },
+            new Course
+            {
+                Id = 2,
+                Name = "Algorithms",
+                StartDate = new DateTime(2026, 3, 15),
+                LecturerId = 2
+            },
+            new Course
+            {
+                Id = 3,
+                Name = "Statistics",
+                StartDate = new DateTime(2026, 4, 10),
+                LecturerId = 3
+            },
+            new Course
+            {
+                Id = 4,
+                Name = "C# Basics",
+                StartDate = new DateTime(2026, 5, 1),
+                LecturerId = 1
+            }
+        };
+
+        public static List<Enrollment> Enrollments { get; } = new List<Enrollment>
+        {
+            new Enrollment
+            {
+                Id = 1,
+                StudentId = 1,
+                CourseId = 1,
+                IsActive = true,
+                FinalGrade = 4.5
+            },
+            new Enrollment
+            {
+                Id = 2,
+                StudentId = 1,
+                CourseId = 2,
+                IsActive = true,
+                FinalGrade = 5.0
+            },
+            new Enrollment
+            {
+                Id = 3,
+                StudentId = 2,
+                CourseId = 1,
+                IsActive = true,
+                FinalGrade = null
+            },
+            new Enrollment
+            {
+                Id = 4,
+                StudentId = 3,
+                CourseId = 3,
+                IsActive = true,
+                FinalGrade = 4.0
+            },
+            new Enrollment
+            {
+                Id = 5,
+                StudentId = 4,
+                CourseId = 2,
+                IsActive = false,
+                FinalGrade = 3.5
+            },
+            new Enrollment
+            {
+                Id = 6,
+                StudentId = 5,
+                CourseId = 4,
+                IsActive = true,
+                FinalGrade = null
+            },
+            new Enrollment
+            {
+                Id = 7,
+                StudentId = 3,
+                CourseId = 1,
+                IsActive = true,
+                FinalGrade = 4.8
+            }
+        };
+    }
--- UniversityDataAnalysis/UniversityDataAnalysis/Program.cs
+++ UniversityDataAnalysis/UniversityDataAnalysis/Program.cs
@@ -1,3 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using UniversityDataAnalysis.Data;
 
-Console.WriteLine("Hello, World!");
+namespace UniversityDataAnalysis;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("=== University Data Analysis ===");
+            Console.WriteLine("0. Show all data");
+            Console.WriteLine("9. Exit");
+            Console.Write("Choose option: ");
+
+            string? option = Console.ReadLine();
+
+            Console.WriteLine();
+
+            switch (option)
+            {
+                case "0":
+                    ShowAllData();
+                    break; 
+                case "9":
+                    return;
+
+                default:
+                    Console.WriteLine("Unknown option.");
+                    break;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key...");
+            Console.ReadKey();
+        }
+    }
+
+    static void ShowAllData()
+    {
+        Console.WriteLine("STUDENTS:");
+        foreach (var s in UniversityData.Students)
+            Console.WriteLine(s);
+
+        Console.WriteLine("\nLECTURERS:");
+        foreach (var l in UniversityData.Lecturers)
+            Console.WriteLine(l);
+
+        Console.WriteLine("\nCOURSES:");
+        foreach (var c in UniversityData.Courses)
+            Console.WriteLine(c);
+
+        Console.WriteLine("\nENROLLMENTS:");
+        foreach (var e in UniversityData.Enrollments)
+            Console.WriteLine(e);
+    }
+}
